bb8f4e3ddb51b5d20c03b7c2ef1f28a57ebb0efa
Helen Brooks <helen.brooks@ukaea.uk>
2024-02-22 11:23:50 +0000

    Moose model can add function objects

--- catbird/model.py
+++ catbird/model.py
@@ -138,6 +138,11 @@ class MooseModel():
         collection_kwargs["collection_type"]=output_type
         self.add_to_collection("Outputs","Output",output_name,**collection_kwargs)
 
+    def add_function(self,function_name,function_type,**kwargs_in):
+        collection_kwargs=deepcopy(kwargs_in)
+        collection_kwargs["collection_type"]=function_type
+        self.add_to_collection("Functions","Function",function_name,**collection_kwargs)
+
     def add_mesh_generator(self,mesh_generator_name,mesh_generator_type,**kwargs_in):
         collection_kwargs=deepcopy(kwargs_in)
         collection_kwargs["collection_type"]=mesh_generator_type
