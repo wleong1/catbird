46d37f3020f000b165d6026f8bc2609d33546d8e
Helen Brooks <helen.brooks@ukaea.uk>
2024-02-22 14:57:24 +0000

    move monoblock model definition into class since it got very long

---
+++ examples/monoblock_thermal_example.py
@@ -0,0 +1,31 @@
+import os
+import subprocess
+import sys
+from models.monoblock import *
+
+def main():
+    # Get path to MOOSE
+    moose_path=os.environ['MOOSE_DIR']
+
+    # Path to executable and inputs
+    module_name="heat_conduction"
+    app_name=module_name+"-opt"
+    app_path=os.path.join(moose_path,"modules",module_name)
+    app_exe=os.path.join(app_path,app_name)
+
+    # Create a factory of available objects from our MOOSE executable
+    factory=MonoblockFactory(app_exe)
+
+    config_name="monoblock_config.json"
+    factory.write_config(config_name)
+
+    # Create a boiler plate MOOSE model from a template
+    model=MonoblockModel(factory)
+    
+    # Write out our input file
+    input_name="monoblock_thermal.i"
+    model.write(input_name)
+
+
+if __name__ == "__main__":
+    main()
